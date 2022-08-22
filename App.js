6613aff47b1cf6b3f7292167b17c812fd51ecb0e
Ambili das v s <ambili.kan@gmail.com>
2022-08-22 18:57:13 +0530

    App.js file

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from './pages/home/home';
+import Orderlogin from './pages/OrderLogin/orderlogin';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Orderlogin /> */}
+      <Home />
+    </div>
+  );
+}
+
+export default App;
