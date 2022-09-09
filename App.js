424b91b3a1815d3ce07cb9f46959f09562af9540
Ambili das v s <ambili.kan@gmail.com>
2022-09-09 20:03:26 +0530

    added redux

--- App.js
+++ App.js
@@ -3,11 +3,15 @@ import Home from './pages/home/home';
 import Orderlogin from './pages/OrderLogin/orderlogin';
 import Router from './components/router/router';
 import Router1 from './components/router/router';
+import { Provider } from 'react-redux';
+import  store  from './components/redux/store';
 
 function App() {
   return (
     <div className="App">
+      <Provider store={store}>
         <Router1 />
+      </Provider>
       {/* <Orderlogin /> 
       <Home /> */}
      
